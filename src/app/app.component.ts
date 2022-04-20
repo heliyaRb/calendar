2fef48f16e66064b34b8715a6013de5dc38cfd3c
Heliya Rahbar <zahrarahbar1991@gmail.com.com>
2022-04-20 16:54:57 +0430

    feat: :art: garegorian and jalali calendar with material calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injector } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'calendar';
+  calendarType: 'miladi' | 'shamsi' = 'shamsi';
+
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+
+  jsonDate = '2018-01-08T20:21:29.4674496';
+  dateControl = this.jsonDate;
+
+  constructor(@Inject(DateAdapter) private dateAdapter: any) {
+    console.log(localStorage.getItem('clr'));
+
+    if (localStorage.getItem('clr') == 'shamsi') {
+      console.log(dateAdapter);
+    } else {
+      console.log(dateAdapter);
+    }
+  }
+
+  changeCalendar() {
+    if (this.calendarType == 'shamsi' || undefined) {
+      this.calendarType = 'miladi';
+      localStorage.setItem('clr', this.calendarType);
+    } else {
+      this.calendarType = 'shamsi';
+      localStorage.setItem('clr', this.calendarType);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule, NativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+
+import {
+  MaterialPersianDateAdapter,
+  PERSIAN_DATE_FORMATS,
+} from './shared/material.persian-date.adapter';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: DateAdapter,
+      useFactory: serviceFactory,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: PERSIAN_DATE_FORMATS },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+export function serviceFactory() {
+  const calendarType: 'shamsi' | 'miladi' | any = localStorage.getItem('clr');
+
+  if (calendarType === 'shamsi') return new MaterialPersianDateAdapter();
+  return new NativeDateAdapter('en-US');
+}
